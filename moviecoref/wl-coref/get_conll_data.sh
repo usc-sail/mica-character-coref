0090ad17e440137746456d928206f85f56f5f87f
sabyasachee <sbaruah@usc.edu>
2022-03-12 17:32:26 +0000

    commented curl commands to allow manual download

--- moviecoref/wl-coref/get_conll_data.sh
+++ moviecoref/wl-coref/get_conll_data.sh
@@ -38,12 +38,12 @@ fi
 
 cd $downloads_dir
 
-curl -O https://cemantix.org/conll/2012/download/conll-2012-train.v4.tar.gz
-curl -O https://cemantix.org/conll/2012/download/conll-2012-development.v4.tar.gz
-curl -O https://cemantix.org/conll/2012/download/test/conll-2012-test-official.v9.tar.gz
-curl -O https://cemantix.org/conll/2012/download/test/conll-2012-test-supplementary.v9.tar.gz
-curl -O https://cemantix.org/conll/2012/download/test/conll-2012-test-key.tar.gz
-curl -O https://cemantix.org/conll/2012/download/conll-2012-scripts.v3.tar.gz
+# curl -O https://cemantix.org/conll/2012/download/conll-2012-train.v4.tar.gz
+# curl -O https://cemantix.org/conll/2012/download/conll-2012-development.v4.tar.gz
+# curl -O https://cemantix.org/conll/2012/download/test/conll-2012-test-official.v9.tar.gz
+# curl -O https://cemantix.org/conll/2012/download/test/conll-2012-test-supplementary.v9.tar.gz
+# curl -O https://cemantix.org/conll/2012/download/test/conll-2012-test-key.tar.gz
+# curl -O https://cemantix.org/conll/2012/download/conll-2012-scripts.v3.tar.gz
 
 cd $original_dir
 
