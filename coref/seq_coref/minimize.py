dc1b246df1fc86e880999162deda7cf71d3192ad
sabyasachee <sabyasachee.1301@gmail.com>
2022-07-31 05:17:10 +0000

    1. Add get_char_vocab.py to get vocabulary of characters 2. Remove external dependencies from minimize.py and use absl for command line argument parsing 3. prepare_data.sh does not run minimize.py and get_char_vocab.py

--- coref/seq_coref/minimize.py
+++ coref/seq_coref/minimize.py
@@ -1,17 +1,16 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import re
 import os
-import sys
 import json
-import tempfile
-import subprocess
 import collections
+from absl import flags
+from absl import app
 
-import util
-import conll
+
+FLAGS = flags.FLAGS
+flags.DEFINE_string("conll_directory", None, "Path to the conll-2012 gold directory",
+ required=True)
+
+BEGIN_DOCUMENT_REGEX = re.compile(r"#begin document \((.*)\); part (\d+)")
 
 class DocumentState(object):
   def __init__(self):
@@ -71,7 +70,7 @@ class DocumentState(object):
       else:
         merged_clusters.append(set(c1))
     merged_clusters = [list(c) for c in merged_clusters]
-    all_mentions = util.flatten(merged_clusters)
+    all_mentions = flatten(merged_clusters)
     assert len(all_mentions) == len(set(all_mentions))
 
     return {
@@ -82,6 +81,12 @@ class DocumentState(object):
       "ner": self.span_dict_to_list(self.ner),
       "clusters": merged_clusters
     }
+
+def flatten(l):
+  return [item for sublist in l for item in sublist]
+
+def get_doc_key(doc_id, part):
+  return "{}_{}".format(doc_id, int(part))
 
 def normalize_word(word, language):
   if language == "arabic":
@@ -123,23 +128,26 @@ def handle_bit(word_index, bit, stack, spans):
     spans[current_span] = label
 
 def handle_line(line, document_state, language, labels, stats):
-  begin_document_match = re.match(conll.BEGIN_DOCUMENT_REGEX, line)
+  begin_document_match = re.match(BEGIN_DOCUMENT_REGEX, line)
   if begin_document_match:
     document_state.assert_empty()
-    document_state.doc_key = conll.get_doc_key(begin_document_match.group(1), begin_document_match.group(2))
+    document_state.doc_key = get_doc_key(begin_document_match.group(1),
+     begin_document_match.group(2))
     return None
   elif line.startswith("#end document"):
     document_state.assert_finalizable()
     finalized_state = document_state.finalize()
     stats["num_clusters"] += len(finalized_state["clusters"])
     stats["num_mentions"] += sum(len(c) for c in finalized_state["clusters"])
-    labels["{}_const_labels".format(language)].update(l for _, _, l in finalized_state["constituents"])
+    labels["{}_const_labels".format(language)].update(
+      l for _, _, l in finalized_state["constituents"])
     labels["ner"].update(l for _, _, l in finalized_state["ner"])
     return finalized_state
   else:
     row = line.split()
     if len(row) == 0:
-      stats["max_sent_len_{}".format(language)] = max(len(document_state.text), stats["max_sent_len_{}".format(language)])
+      stats["max_sent_len_{}".format(language)] = max(len(document_state.text),
+       stats["max_sent_len_{}".format(language)])
       stats["num_sents_{}".format(language)] += 1
       document_state.sentences.append(tuple(document_state.text))
       del document_state.text[:]
@@ -148,7 +156,6 @@ def handle_line(line, document_state, language, labels, stats):
       return None
     assert len(row) >= 12
 
-    doc_key = conll.get_doc_key(row[0], row[1])
     word = normalize_word(row[3], language)
     parse = row[5]
     speaker = row[9]
@@ -178,8 +185,8 @@ def handle_line(line, document_state, language, labels, stats):
     return None
 
 def minimize_partition(name, language, extension, labels, stats):
-  input_path = "{}.{}.{}".format(name, language, extension)
-  output_path = "{}.{}.jsonlines".format(name, language)
+  input_path = os.path.join(FLAGS.conll_directory, "{}.{}.{}".format(name, language, extension))
+  output_path = os.path.join(FLAGS.conll_directory, "{}.{}.jsonlines".format(name, language))
   count = 0
   print("Minimizing {}".format(input_path))
   with open(input_path, "r") as input_file:
@@ -199,7 +206,7 @@ def minimize_language(language, labels, stats):
   minimize_partition("train", language, "v4_gold_conll", labels, stats)
   minimize_partition("test", language, "v4_gold_conll", labels, stats)
 
-if __name__ == "__main__":
+def main(argv):
   labels = collections.defaultdict(set)
   stats = collections.defaultdict(int)
   minimize_language("english", labels, stats)
@@ -209,3 +216,6 @@ if __name__ == "__main__":
     print("{} = [{}]".format(k, ", ".join("\"{}\"".format(label) for label in v)))
   for k, v in stats.items():
     print("{} = {}".format(k, v))
+
+if __name__=="__main__":
+  app.run(main)
--- coref/seq_coref/prepare_data.sh
+++ coref/seq_coref/prepare_data.sh
@@ -77,11 +77,6 @@ compile_languages() {
 	compile_language arabic
 }
 
-# download_conll_2012
-# convert_skeleton_to_conll
-# compile_languages
-# python minimize.py
-# python get_char_vocab.py
-
-# python filter_embeddings.py glove.840B.300d.txt train.english.jsonlines dev.english.jsonlines
-# python cache_elmo.py train.english.jsonlines dev.english.jsonlines
+download_conll_2012
+convert_skeleton_to_conll
+compile_languages
