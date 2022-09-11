30b44af2917c9b8924436a9f1858e870fb191602
sabyasachee <sbaruah@usc.edu>
2022-09-11 00:40:49 +0000

    Add forward propagation method.

--- coref/seq_coref/coref_longformer.py
+++ coref/seq_coref/coref_longformer.py
@@ -1,8 +1,10 @@
 """Longformer model."""
 
+import numpy as np
+import torch
 from torch import nn
 from transformers import LongformerTokenizer, LongformerModel
-
+from transformers.models.longformer import modeling_longformer
 
 class CorefLongformerModel(nn.Module):
     """Coreference Resolution Model for English using the Longformer model.
@@ -26,6 +28,44 @@ class CorefLongformerModel(nn.Module):
         self.gru_hidden_size = self.longformer_hidden_size
         self.gru = nn.GRU(
             self.longformer_hidden_size + self.label_embedding_size,
-            self.gru_hidden_size, bidirectional=True)
-        self.token_classifier = nn.Linear(self.gru_hidden_size, self.n_labels)
-        
+            self.gru_hidden_size, batch_first=True, bidirectional=True)
+        self.token_classifier = nn.Linear(2 * self.gru_hidden_size, 
+                                          self.n_labels)
+    
+    def forward(self, 
+                token_ids: torch.LongTensor, 
+                mention_ids: torch.IntTensor, 
+                attn_mask: torch.FloatTensor, 
+                global_attn_mask: torch.FloatTensor, 
+                label_ids: torch.IntTensor | None = None) -> (
+                tuple[torch.IntTensor, torch.FloatTensor] | torch.IntTensor):
+        """Forward propagation. If label_ids is None, return predictions only.
+        Otherwise, return both predictions and loss, which is used for
+        backpropagation.
+        """
+        device = next(self.parameters()).device
+        longformer_output: (
+            modeling_longformer.LongformerBaseModelOutputWithPooling) = (
+                self.longformer(token_ids, attn_mask, global_attn_mask))
+        longformer_hidden: torch.FloatTensor = (
+            longformer_output.last_hidden_state)
+        mention_embedding: torch.FloatTensor = self.label_embedding(mention_ids)
+        gru_input: torch.FloatTensor = torch.cat(
+            (longformer_hidden, mention_embedding), dim=2)
+        gru_output: torch.FloatTensor = self.gru(gru_input)[0]
+        logits: torch.FloatTensor = self.token_classifier(gru_output).int()
+        predictions: torch.IntTensor = logits.argmax(dim=2)
+
+        if label_ids is not None:
+            active_labels = label_ids[attn_mask == 1.]
+            active_logits = logits.flatten(0, 1)[attn_mask.flatten() == 1.]
+            label_distribution = np.bincount(active_labels, 
+                                             minlength=self.n_labels)
+            class_weight = torch.FloatTensor(1/(1 + label_distribution)).to(
+                device)
+            cross_entrop_loss_fn = nn.CrossEntropyLoss(weight=class_weight, 
+                                                       reduction="mean")
+            loss = cross_entrop_loss_fn(active_logits, active_labels)
+            return predictions, loss
+        else:
+            return predictions 
