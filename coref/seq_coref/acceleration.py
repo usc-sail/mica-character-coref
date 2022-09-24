8322d90440257af100aee4a72e46876c52fd8516
sabyasachee <sbaruah@usc.edu>
2022-09-24 01:59:47 +0000

    Check if flags are passed

--- coref/seq_coref/acceleration.py
+++ coref/seq_coref/acceleration.py
@@ -3,11 +3,13 @@
 from absl import flags
 import accelerate
 from accelerate import logging as alogging
-import logging
 
 FLAGS = flags.FLAGS
-mixed_precision = "fp16" if FLAGS.use_mixed_precision else "no"
+mixed_precision = "fp16" if ("mixed_precision" in FLAGS and 
+                             FLAGS.use_mixed_precision) else "no"
+grad_accumulation_steps = (
+    FLAGS.grad_accumulation_steps if "grad_accumulation_steps" in FLAGS else 1)
 accelerator = accelerate.Accelerator(
-    gradient_accumulation_steps=FLAGS.grad_accumulation_steps,
+    gradient_accumulation_steps=grad_accumulation_steps,
     mixed_precision=mixed_precision)
 logger = alogging.get_logger("")
