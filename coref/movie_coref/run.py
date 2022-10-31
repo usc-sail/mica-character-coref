ddbb53125ee1f3d51c2fc2a8f0c4d9a91941d212
sabyasachee <sbaruah@usc.edu>
2022-10-31 07:34:50 +0000

    Command-line argument for reference scorer path. Log to latest.log when debugging.

--- coref/movie_coref/run.py
+++ coref/movie_coref/run.py
@@ -12,89 +12,81 @@ FLAGS = flags.FLAGS
 proj_dir = os.getcwd()
 
 # Input and Output
-flags.DEFINE_string(
-    "input_dir", 
-    os.path.join(proj_dir, "data/movie_coref/results"),
-    "Directory of train and dev jsonlines.")
-flags.DEFINE_enum(
-    "input_type", "regular", ["regular", "nocharacters", "addsays"], 
-    "Type of preprocessing applied to screenplays.")
-flags.DEFINE_string(
-    "weights_file", 
-    os.path.join(proj_dir, "data/word_level_coref/data/"
-        "roberta_(e20_2021.05.02_01.16)_release.pt"),
-    "Filepath of word-level coreference roberta model's weights.")
-flags.DEFINE_string(
-    "output_dir", 
-    os.path.join(proj_dir, "data/movie_coref/results/coreference"),
-    "Directory to save model weights, predictions, evaluation metrics, and "
-    "loss curves.")
-flags.DEFINE_string(
-    "logs_dir", 
-    os.path.join(proj_dir, "data/movie_coref/results/coreference/logs"),
-    "Directory to save logs.")
-flags.DEFINE_bool("save_model", False, "Save model weights.")
-flags.DEFINE_bool("save_tensors", False, "Save predictions.")
-flags.DEFINE_bool("save_loss_curves", False, "Save loss curves.")
-flags.DEFINE_bool("debug", False, "Debug output.")
+flags.DEFINE_string("input_dir", default=os.path.join(proj_dir, "data/movie_coref/results"), 
+    help="Directory of train and dev jsonlines.")
+flags.DEFINE_enum("input_type", default="regular", enum_values=["regular", 
+    "nocharacters", "addsays"], help="Type of preprocessing applied to screenplays.")
+flags.DEFINE_string("weights_file", default=os.path.join(proj_dir, "data/word_level_coref/data/"
+        "roberta_(e20_2021.05.02_01.16)_release.pt"), help=("Filepath of word-level coreference "
+        "roberta model's weights."))
+flags.DEFINE_string("output_dir", default=os.path.join(proj_dir, 
+    "data/movie_coref/results/coreference"), help=("Directory to save model weights, predictions, "
+    "evaluation metrics, and loss curves."))
+flags.DEFINE_string("logs_dir", default=os.path.join(proj_dir, 
+    "data/movie_coref/results/coreference/logs"), help="Directory to save logs.")
+flags.DEFINE_string("reference_scorer", default=os.path.join(proj_dir, 
+    "coref/movie_coref/scorer/v8.01/scorer.pl"), help="Path to conll reference scorer.")
+flags.DEFINE_bool("save_model", default=False, help="Save model weights.")
+flags.DEFINE_bool("save_tensors", default=False, help="Save predictions.")
+flags.DEFINE_bool("save_loss_curves", default=False, help="Save loss curves.")
+flags.DEFINE_bool("debug", default=False, help="Debug output.")
 
 # Training
-flags.DEFINE_bool("freeze_bert", False, help="Freeze RoBerta.")
-flags.DEFINE_enum(
-    "genre", "wb", ["bc", "bn", "mz", "nw", "pt", "tc", "wb"], "Genre.")
-flags.DEFINE_float("bce_weight", 0.5, "Weight of BCE coreference loss.")
-flags.DEFINE_float("bert_lr", 1e-5, "Learning rate for the transformer.")
-flags.DEFINE_float("lr", 3e-4, "Learning rate for the model.")
-flags.DEFINE_float("weight_decay", 1e-3, "Weight decay.")
-flags.DEFINE_integer("max_epochs", 10, "Maximum number of epochs.")
-flags.DEFINE_float("dropout", 0.3, "Dropout rate.")
-flags.DEFINE_integer(
-    "document_len", 5120, 
-    "Length of document in words the screenplay is split into.")
-flags.DEFINE_integer(
-    "overlap_len", 512, 
-    "Number of words overlapping between successive documents of a screenplay.")
-flags.DEFINE_integer(
-    "subword_batch_size", 2, "Batch size of subword sequences for encoding.")
-flags.DEFINE_integer(
-    "cr_batch_size", 16, 
-    "Batch size of word sequences for character head recognition.")
-flags.DEFINE_integer(
-    "fn_batch_size", 16, "Batch size of word pairs for fine scoring.")
-flags.DEFINE_integer(
-    "cr_seq_len", 256, 
-    "Sequence length of word sequences for character head recognition.")
-flags.DEFINE_bool(
-    "run_span", False, "Train and evaluate the span predictor module.")
+flags.DEFINE_bool("freeze_bert", default=False, help="Freeze RoBerta.")
+flags.DEFINE_enum("genre", default="wb", enum_values=["bc", "bn", "mz", "nw", "pt", "tc", "wb"], 
+    help="Genre.")
+flags.DEFINE_float("bce_weight", default=0.5, help="Weight of BCE coreference loss.")
+flags.DEFINE_float("bert_lr", default=1e-5, help="Learning rate for the transformer.")
+flags.DEFINE_float("lr", default=3e-4, help="Learning rate for the model.")
+flags.DEFINE_float("weight_decay", default=1e-3, help="Weight decay.")
+flags.DEFINE_integer("max_epochs", default=10, help="Maximum number of epochs.")
+flags.DEFINE_float("dropout", default=0.3, help="Dropout rate.")
+flags.DEFINE_integer("train_document_len", default=5120, 
+    help="Length of document in words the training screenplay is split into.")
+flags.DEFINE_integer("eval_document_len", default=None, help=("Length of document in words the "
+    "evaluation screenplay is split into. -1 means the screenplay is not split"))
+flags.DEFINE_integer("eval_document_overlap_len", default=512, help=("Number of words overlapping "
+    "between successive documents of an evaluation screenplay."))
+flags.DEFINE_integer("subword_batch_size", default=2, 
+    help="Batch size of subword sequences for encoding.")
+flags.DEFINE_integer("cr_batch_size", default=16, 
+    help="Batch size of word sequences for character head recognition.")
+flags.DEFINE_integer("fn_batch_size", default=16, help="Batch size of word pairs for fine scoring.")
+flags.DEFINE_integer("cr_seq_len", default=256, 
+    help="Sequence length of word sequences for character head recognition.")
+flags.DEFINE_bool("run_span", default=False, help="Train and evaluate the span predictor module.")
 
 # Model
-flags.DEFINE_integer(
-    "topk", 50, 
-    "Maximum number of preceding antecedents to retain after coarse scoring.")
-flags.DEFINE_integer("gru_nlayers", 1, "Number of GRU layers.")
-flags.DEFINE_integer("gru_hidden_size", 256, "Hidden size of GRU.")
-flags.DEFINE_bool("gru_bi", True, "Bidirectional GRU layers.")
-flags.DEFINE_integer("tag_embedding_size", 16, "Tag embedding size.")
+flags.DEFINE_integer("topk", default=50, 
+    help="Maximum number of preceding antecedents to retain after coarse scoring.")
+flags.DEFINE_integer("gru_nlayers", default=1, help="Number of GRU layers.")
+flags.DEFINE_integer("gru_hidden_size", default=256, help="Hidden size of GRU.")
+flags.DEFINE_bool("gru_bi", default=True, help="Bidirectional GRU layers.")
+flags.DEFINE_integer("tag_embedding_size", default=16, help="Tag embedding size.")
 
 # Validators
-flags.register_validator(
-    "document_len", lambda x: x in [2048, 3072, 4096, 5120], 
-    "document_len should be 2048, 3072, 4096, or 5120")
-flags.register_validator(
-    "overlap_len", lambda x: x in [128, 256, 384, 512], 
-    "overlap_len should be 128, 256, 384, or 512")
+flags.register_validator("train_document_len", lambda x: x in [2048, 3072, 4096, 5120], 
+    "train_document_len should be one of 2048, 3072, 4096, or 5120")
+flags.register_validator("eval_document_len", lambda x: x is None or x in [2048, 3072, 4096, 5120], 
+    "eval_document_len should be None or one of 2048, 3072, 4096, or 5120")
+flags.register_validator("eval_document_overlap_len", lambda x: x in [128, 256, 384, 512], 
+    "eval_document_overlap_len should be one of 128, 256, 384, or 512")
 
 def main(argv):
     if len(argv) > 1:
         print(f"Unnecessary command-line arguments: {argv}")
-    time = (datetime.datetime.now(pytz.timezone("America/Los_Angeles"))
-            .strftime("%b%d_%I:%M:%S%p"))
-    log_file = os.path.join(FLAGS.logs_dir, f"latest.log")
-    train_file = os.path.join(
-        FLAGS.input_dir, FLAGS.input_type, "train.jsonlines")
+    if FLAGS.debug:
+        log_file = os.path.join(FLAGS.logs_dir, f"latest.log")
+    else:
+        time = (datetime.datetime.now(pytz.timezone("America/Los_Angeles")).strftime(
+            "%b%d_%I:%M:%S%p"))
+        log_file = os.path.join(FLAGS.logs_dir, f"{time}.log")
+    train_file = os.path.join(FLAGS.input_dir, FLAGS.input_type, "train.jsonlines")
     dev_file = os.path.join(FLAGS.input_dir, FLAGS.input_type, "dev.jsonlines")
     trainer = CoreferenceTrainer(
         log_file=log_file,
+        output_dir=FLAGS.output_dir,
+        reference_scorer=FLAGS.reference_scorer,
         tag_embedding_size=FLAGS.tag_embedding_size,
         gru_nlayers=FLAGS.gru_nlayers,
         gru_hidden_size=FLAGS.gru_hidden_size,
@@ -111,8 +103,9 @@ def main(argv):
         lr=FLAGS.lr,
         weight_decay=FLAGS.weight_decay,
         max_epochs=FLAGS.max_epochs,
-        document_len=FLAGS.document_len,
-        overlap_len=FLAGS.overlap_len,
+        train_document_len=FLAGS.train_document_len,
+        eval_document_len=FLAGS.eval_document_len,
+        eval_document_overlap_len=FLAGS.eval_document_overlap_len,
         cr_seq_len=FLAGS.cr_seq_len,
         subword_batch_size=FLAGS.subword_batch_size,
         cr_batch_size=FLAGS.cr_batch_size,
@@ -122,7 +115,7 @@ def main(argv):
         save_output=FLAGS.save_tensors,
         save_loss_curve=FLAGS.save_loss_curves,
         debug=FLAGS.debug)
-    trainer.run()
+    trainer()
 
 if __name__=="__main__":
     app.run(main)
