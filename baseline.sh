23a8f30a382446aeaeeee277f92519e7b79c99e1
sabyasachee <sbaruah@usc.edu>
2022-11-06 16:32:10 +0000

    Running baseline on train scripts on cpu

--- baseline.sh
+++ baseline.sh
@@ -7,11 +7,10 @@ merge=("" "no")
 gold_mention=("" "no")
 gold_singleton=("" "no")
 
-rm -f data/movie_coref/results/coreference/baselines/baseline.tsv
+rm -f data/movie_coref/results/coreference/baselines/train.baseline.tsv
 echo -e "preprocess\tgenre\tentity\tmerge_speakers\tprovide_gold_mentions\tremove_gold_singletons\
-\tmuc_precision\tmuc_recall\tmuc_f1\tb_cubed_precision\tb_cubed_recall\tb_cubed_f1\
-\tceafe_precision\tceafe_recall\tceafe_f1\taverage_f1" > \
-data/movie_coref/results/coreference/baselines/baseline.tsv
+\tmetric\tmovie\tprecision\trecall\tf1" > \
+data/movie_coref/results/coreference/baselines/train.baseline.tsv
 for preprocess in "${preprocess_vals[@]}"; do
 for entity in "${entity_vals[@]}"; do
 for genre in "${genres[@]}"; do
@@ -24,5 +23,6 @@ if [ -z "$gs" ]; then gsx="yes"; else gsx="no"; fi
 echo -e "preprocess=${preprocess} entity=${entity} genre=${genre} merge_speakers=${mgx} \
 provide_gold_mentions=${gmx} remove_gold_singletons=${gsx}"
 python coref/movie_coref/baseline_main.py --preprocess=$preprocess --entity=$entity \
---wl_genre=$genre --${mg}merge_speakers --${gm}provide_gold_mentions --${gs}remove_gold_singletons
+--wl_genre=$genre --${mg}merge_speakers --${gm}provide_gold_mentions --${gs}remove_gold_singletons \
+--append_results --run_train
 done; done; done; done; done; done
