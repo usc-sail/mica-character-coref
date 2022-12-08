c491a8fe8fac17d4f8cadc844c92d5a6beee9d19
sabyasachee <sabyasachee.1301@gmail.com>
2022-12-08 14:24:47 -0800

    formatting changes and add arguments for cross validation

--- coref/movie_coref/run.py
+++ coref/movie_coref/run.py
@@ -12,13 +12,21 @@ proj_dir = os.path.join(os.getenv("PROJ_DIR"), "mica_text_coref")
 data_dir = os.path.join(os.getenv("DATA_DIR"), "mica_text_coref")
 
 # Directories and Files
-flags.DEFINE_string("input_dir", default=os.path.join(data_dir, "movie_coref/results"), help="directory containing subdirectories of preprocessed train and dev script jsonlines")
-flags.DEFINE_enum("input_type", default="regular", enum_values=["regular", "nocharacters", "addsays"], help="type of preprocessing applied to screenplays")
-flags.DEFINE_string("weights_file", default=os.path.join(data_dir, "word_level_coref/data/roberta_(e20_2021.05.02_01.16)_release.pt"), 
+flags.DEFINE_string("input_dir", default=os.path.join(data_dir, "movie_coref/results"),
+                    help="directory containing subdirectories of preprocessed train and dev script jsonlines")
+flags.DEFINE_enum("input_type", default="regular", enum_values=["regular", "nocharacters", "addsays"],
+                    help="type of preprocessing applied to screenplays")
+flags.DEFINE_string("weights_file",
+                    default=os.path.join(data_dir, "word_level_coref/data/roberta_(e20_2021.05.02_01.16)_release.pt"), 
                     help="filepath of word-level coreference roberta model's weights")
-flags.DEFINE_string("output_dir", default=os.path.join(data_dir, "movie_coref/results/coreference"), help="directory to save model weights, predictions, metrics, loss curves, and logs")
-flags.DEFINE_string("reference_scorer", default=os.path.join(proj_dir, "coref/movie_coref/scorer/v8.01/scorer.pl"), help="path of conll reference scorer")
-flags.DEFINE_enum("test_movie", default="none", enum_values=["avengers_endgame", "dead_poets_society", "john_wick", "prestige", "quiet_place", "zootopia", "none"], help="test movie")
+flags.DEFINE_string("output_dir", default=os.path.join(data_dir, "movie_coref/results/coreference"),
+                    help="directory to save model weights, predictions, metrics, loss curves, and logs")
+flags.DEFINE_string("reference_scorer", default=os.path.join(proj_dir, "coref/movie_coref/scorer/v8.01/scorer.pl"),
+                    help="path of conll reference scorer")
+flags.DEFINE_enum("test_movie", default="none",
+                  enum_values=["avengers_endgame", "dead_poets_society", "john_wick", "prestige", "quiet_place",
+                               "zootopia", "none"],
+                  help="test movie")
 
 # Training
 flags.DEFINE_bool("freeze_bert", default=False, help="freeze RoBerta transformer")
@@ -27,25 +35,37 @@ flags.DEFINE_float("bce_weight", default=0.5, help="weight of the BCE coreferenc
 flags.DEFINE_float("bert_lr", default=2e-5, help="learning rate of the transformer")
 flags.DEFINE_float("character_lr", default=2e-4, help="learning rate of the character recognition model")
 flags.DEFINE_float("coref_lr", default=2e-4, help="learning rate of the coreference model")
-flags.DEFINE_float("warmup", default=-1, help="number of warmup epochs when the learning rate increases from 0 to max, can be a fraction. if -1, learning rate is constant")
+flags.DEFINE_float("warmup", default=-1,
+                   help=("number of warmup epochs when the learning rate increases from 0 to max, can be a fraction."
+                         " if -1, learning rate is constant"))
 flags.DEFINE_float("weight_decay", default=1e-3, help="weight decay")
 flags.DEFINE_integer("max_epochs", default=20, help="maximum number of epochs to train the model")
-flags.DEFINE_integer("patience", default=3, help="maximum number of epochs to wait for dev performance to improve until early-stopping")
+flags.DEFINE_integer("patience", default=5,
+                     help="maximum number of epochs to wait for dev performance to improve until early-stopping")
 flags.DEFINE_float("dropout", default=0, help="dropout rate")
 flags.DEFINE_integer("train_document_len", default=5120, help="length of training subdocument in words")
-flags.DEFINE_integer("test_document_len", default=5120, help="length of testing subdocument in words")
-flags.DEFINE_integer("test_overlap_len", default=512, help="length of overlap between successive testing subdocuments in words")
-flags.DEFINE_enum("test_merge_strategy", default="mean", enum_values=["pre", "post", "max", "min", "mean", "none"], help="strategy to join coref scores of adjacent test subdocuments")
+flags.DEFINE_multi_integer("test_document_lens", default=[2048, 3072, 4096, 5120],
+                           help="length of testing subdocument in words")
+flags.DEFINE_multi_integer("test_overlap_lens", default=[0, 128, 256, 384, 512],
+                           help="length of overlap between successive testing subdocuments in words")
+flags.DEFINE_multi_enum("test_merge_strategies", default=["none", "pre", "post", "max", "min", "avg"],
+                        enum_values=["none", "pre", "post", "max", "min", "avg"],
+                        help="strategy to join coref scores of adjacent test subdocuments")
 flags.DEFINE_integer("subword_batch_size", default=64, help="batch size of subword sequences")
 flags.DEFINE_integer("cr_batch_size", default=64, help="batch size of word sequences for character head recognition")
 flags.DEFINE_integer("fn_batch_size", default=64, help="batch size of word pairs for fine scoring")
 flags.DEFINE_integer("sp_batch_size", default=64, help="batch size of head ids for span prediction")
 flags.DEFINE_integer("cr_seq_len", default=256, help="sequence length of word sequences for character head recognition")
-flags.DEFINE_bool("add_cr_to_coarse", default=True, help="if true, add character scores to the coarse scores for top antecedent selection")
-flags.DEFINE_bool("filter_by_cr", default=False, help="if true, filter antecedents by the predicted character heads for finding predicted word-level clusters")
-flags.DEFINE_bool("remove_singleton_cr", default=True, help="if true, remove predicted word-level clusters containing a single predicted character head")
+flags.DEFINE_bool("add_cr_to_coarse", default=True,
+                  help="if true, add character scores to the coarse scores for top antecedent selection")
+flags.DEFINE_bool("filter_by_cr", default=False,
+                  help=("if true, filter antecedents by the predicted character heads for finding predicted "
+                        "word-level clusters"))
+flags.DEFINE_bool("remove_singleton_cr", default=True,
+                  help="if true, remove predicted word-level clusters containing a single predicted character head")
 flags.DEFINE_bool("eval_train", default=True, help="if true, evaluate training set")
-flags.DEFINE_integer("n_epochs_no_eval", default=0, help="initial number of epochs for which no evaluation is performed")
+flags.DEFINE_integer("n_epochs_no_eval", default=0,
+                     help="initial number of epochs for which no evaluation is performed")
 flags.DEFINE_bool("save_log", default=False, help="if true, save log")
 flags.DEFINE_bool("save_model", default=False, help="if true, save model weights")
 flags.DEFINE_bool("save_predictions", default=False, help="if true, save predictions")
@@ -61,8 +81,8 @@ flags.DEFINE_bool("gru_bi", default=True, help="bidirectional GRU layers")
 flags.DEFINE_integer("tag_embedding_size", default=16, help="tag embedding size")
 
 # Validators
-flags.register_validator("train_document_len", lambda x: x in [1024, 2048, 3072, 4096, 5120], message="train_document_len should be one of 1024, 2048, 3072, 4096, or 5120")
-flags.register_multi_flags_validator(["save", "debug"], lambda x: not(x["save"] and x["debug"]), "debug and save cannot both be true")
+flags.register_multi_flags_validator(["save", "debug"], lambda x: not(x["save"] and x["debug"]),
+                                     message="debug and save cannot both be true")
 
 def main(argv):
     if len(argv) > 1:
@@ -70,7 +90,8 @@ def main(argv):
         return
     time = datetime.datetime.now(pytz.timezone("America/Los_Angeles")).strftime("%b%d_%I:%M:%S%p")
     output_dir = os.path.join(FLAGS.output_dir, time)
-    if FLAGS.test_movie != "none": output_dir += "_" + FLAGS.test_movie
+    if FLAGS.test_movie != "none":
+        output_dir += "_" + FLAGS.test_movie
     train_file = os.path.join(FLAGS.input_dir, FLAGS.input_type, "train.jsonlines")
     dev_file = os.path.join(FLAGS.input_dir, FLAGS.input_type, "dev.jsonlines")
     trainer = CoreferenceTrainer(
@@ -98,9 +119,9 @@ def main(argv):
         max_epochs=FLAGS.max_epochs,
         patience=FLAGS.patience,
         train_document_len=FLAGS.train_document_len,
-        test_document_len=FLAGS.test_document_len,
-        test_overlap_len=FLAGS.test_overlap_len,
-        test_merge_strategy=FLAGS.test_merge_strategy,
+        test_document_lens=FLAGS.test_document_lens,
+        test_overlap_lens=FLAGS.test_overlap_lens,
+        test_merge_strategies=FLAGS.test_merge_strategies,
         subword_batch_size=FLAGS.subword_batch_size,
         cr_seq_len=FLAGS.cr_seq_len,
         cr_batch_size=FLAGS.cr_batch_size,
