7ff866b6215de60fc7571bfc4ac1c34f0d693496
sabyasachee <sabyasachee.1301@gmail.com>
2022-12-08 13:45:30 -0800

    rearrange bash run scripts into experiments

---
+++ experiments/cross_val/run_cross_vals.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sbatch carc/job.sh avengers_endgame 10240
+sbatch carc/job.sh dead_poets_society 10240
+sbatch carc/job.sh john_wick 10240
+sbatch carc/job.sh prestige 10240
+sbatch carc/job.sh quiet_place 10240
+sbatch carc/job.sh zootopia 10240
+
+sbatch carc/job.sh avengers_endgame 20480
+sbatch carc/job.sh dead_poets_society 20480
+sbatch carc/job.sh john_wick 20480
+sbatch carc/job.sh prestige 20480
+sbatch carc/job.sh quiet_place 20480
+sbatch carc/job.sh zootopia 20480
