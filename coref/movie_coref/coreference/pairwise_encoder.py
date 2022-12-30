f798abf318619323d6ab4abe45a2f111db18bbb0
sabyasachee <sabyasachee.1301@gmail.com>
2022-12-30 01:30:48 -0800

    add option to provide the word ids

--- coref/movie_coref/coreference/pairwise_encoder.py
+++ coref/movie_coref/coreference/pairwise_encoder.py
@@ -32,23 +32,22 @@ class PairwiseEncoder(nn.Module):
     def forward(self,  
                 top_indices: torch.Tensor,
                 speakers: list[str],
-                genre: str) -> torch.Tensor:
-        word_ids = torch.arange(0, len(top_indices), device=self.device)
-        speaker_map = torch.tensor(
-            self._speaker_map(speakers), device=self.device)
-        same_speaker = (speaker_map[top_indices] == speaker_map.unsqueeze(1))
+                genre: str,
+                word_ids: torch.Tensor = None) -> torch.Tensor:
+        if word_ids is None:
+            word_ids = torch.arange(0, len(top_indices), device=self.device)
+        speaker_map = torch.tensor(self._speaker_map(speakers), device=self.device)
+        same_speaker = (speaker_map[top_indices] == speaker_map[word_ids].unsqueeze(1))
         same_speaker = self.speaker_emb(same_speaker.to(torch.long))
 
         # bucketing the distance (see __init__())
-        distance = (
-            word_ids.unsqueeze(1) - word_ids[top_indices]).clamp_min_(min=1)
+        distance = (word_ids.unsqueeze(1) - top_indices).abs_().clamp_min_(min=1)
         log_distance = distance.to(torch.float).log2().floor_()
         log_distance = log_distance.clamp_max_(max=6).to(torch.long)
         distance = torch.where(distance < 5, distance - 1, log_distance + 2)
         distance = self.distance_emb(distance)
 
-        genre = torch.tensor(
-            self.genre2int[genre], device=self.device).expand_as(top_indices)
+        genre = torch.tensor(self.genre2int[genre], device=self.device).expand_as(top_indices)
         genre = self.genre_emb(genre)
 
         return self.dropout(torch.cat((same_speaker, distance, genre), dim=2))
