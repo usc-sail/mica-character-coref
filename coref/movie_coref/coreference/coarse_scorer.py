73dc55297bc15d501d25ba66082def8eb3c16c72
sabyasachee <sabyasachee.1301@gmail.com>
2022-12-30 01:29:20 -0800

    add option to score succeeding words and remove the pruning stage

--- coref/movie_coref/coreference/coarse_scorer.py
+++ coref/movie_coref/coreference/coarse_scorer.py
@@ -19,16 +19,39 @@ class CoarseScorer(nn.Module):
         self.to(device)
         self._device = device
     
-    def forward(self, word_embeddings: torch.Tensor, character_scores: torch.Tensor = None) -> tuple[torch.Tensor, torch.Tensor]:
+    def forward(self, word_embeddings: torch.Tensor, character_scores: torch.Tensor = None,
+                score_succeeding: bool = False, prune: bool = True) -> tuple[torch.Tensor, torch.Tensor] | torch.Tensor:
+        """Forward propagation of coarse scoring module
+
+        Args:
+            word_embeddings (Tensor[n, d]): word embeddings
+            character_scores (Tensor[n]): word-wise character scores. If None, then it is not added to final score
+            score_succeeding (bool): score future words, otherwise put -inf
+            prune (bool): retain topk scoring words
+        
+        Returns:
+            if prune
+                top_scores (Tensor[n, k]): top scores
+                indices (Tensor[n, k]): indices of the top-scoring words
+            else
+                scores (Tensor[n, n]): scores
+        """
         # [n_words, n_words]
-        pair_mask = torch.arange(word_embeddings.shape[0])
-        pair_mask = pair_mask.unsqueeze(1) - pair_mask.unsqueeze(0)
-        pair_mask = torch.log((pair_mask > 0).to(torch.float))
-        pair_mask = pair_mask.to(self.device)
+        if score_succeeding:
+            pair_mask = 0
+        else:
+            pair_mask = torch.arange(word_embeddings.shape[0])
+            pair_mask = pair_mask.unsqueeze(1) - pair_mask.unsqueeze(0)
+            pair_mask = torch.log((pair_mask > 0).to(torch.float))
+            pair_mask = pair_mask.to(self.device)
         bilinear_scores = self.dropout(self.bilinear(word_embeddings)).mm(word_embeddings.T)
         if character_scores is not None:
-            rough_scores = (pair_mask + bilinear_scores + character_scores.unsqueeze(dim=1) + character_scores.unsqueeze(dim=0))
+            rough_scores = (pair_mask + bilinear_scores + character_scores.unsqueeze(dim=1)
+                            + character_scores.unsqueeze(dim=0))
         else:
             rough_scores = pair_mask + bilinear_scores
-        top_scores, indices = torch.topk(rough_scores, k=min(self.topk, len(rough_scores)), dim=1, sorted=False)
-        return top_scores, indices
+        if prune:
+            top_scores, indices = torch.topk(rough_scores, k=min(self.topk, len(rough_scores)), dim=1, sorted=False)
+            return top_scores, indices
+        else:
+            return rough_scores
